19ee7c8dd5065b17e5227bd872e1a8aee27ef241
jbprojectlab <james.byrd1265@gmail.com>
2022-06-16 20:46:24 -0400

    feat: scroll to section

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react'
+import React, {useRef, Fragment} from 'react'
 import Carousel from './components/carousel'
 import Navbar from './components/navbar'
 import ContentBox from './components/content-box'
@@ -7,58 +7,74 @@ import './css/app.css'
 import ContactInfo from './components/contact-info'
 import Footer from './components/footer'
 
-const services = [
-  {
-    title: 'On Site Crating/Uncrating',
-    rows: ['Solid/slate', 'Art work', 'Electronics', 'Marble/glass']
-  },
-  {
-    title: 'Specialty Services',
-    rows: ['Pianos', 'Grandfather clocks', 'Pool tables', 'TV mounting', 'Artwork hanging', 'Light fixtures']
-  },
-  {
-    title: 'Assembling/Disassembling',
-    rows: ['Appliances services', 'Furniture', 'Exercise equipment']
-  }
-]
 
-const App = () => (
-  <Fragment>
-    <Navbar />
-    <Carousel />
-    <div className='content-boxes-container'>
-      <ContentBox title="Services" iconSrc='img/box-icon.png'>
-        <div className='services-container'>
-          <ul>
-            {services.length && services.map(({title, rows}, idx) => (
-              <Fragment key={title + String(idx)}>
-                <li className='service-title'>{title}</li>
-                <ul>
-                  {rows.map((row, idx) => (
-                    <li key={row + String(idx)}>{row}</li>
-                  ))}
-                </ul>
-              </Fragment>
-            ))}
-          </ul>
+const App = () => {
+  const services = [
+    {
+      title: 'On Site Crating/Uncrating',
+      rows: ['Solid/slate', 'Art work', 'Electronics', 'Marble/glass']
+    },
+    {
+      title: 'Specialty Services',
+      rows: ['Pianos', 'Grandfather clocks', 'Pool tables', 'TV mounting', 'Artwork hanging', 'Light fixtures']
+    },
+    {
+      title: 'Assembling/Disassembling',
+      rows: ['Appliances services', 'Furniture', 'Exercise equipment']
+    }
+  ]
+  
+  const servicesRef = useRef(null)
+  const aboutRef = useRef(null)
+  const contactRef = useRef(null)
+
+  const scrollRefs = [servicesRef, aboutRef, contactRef]
+  const scrollToRef = (section) => scrollRefs[section].current.scrollIntoView({behavior: 'smooth'})
+
+  return (
+    <Fragment>
+      <Navbar scrollToSection={scrollToRef} />
+      <Carousel />
+      <div className='content-boxes-container'>
+        <div className='content-box-container' ref={servicesRef}>
+          <ContentBox title="Services" iconSrc='img/box-icon.png'>
+            <div className='services-container'>
+              <ul>
+                {services.length && services.map(({title, rows}, idx) => (
+                  <Fragment key={title + String(idx)}>
+                    <li className='service-title'>{title}</li>
+                    <ul>
+                      {rows.map((row, idx) => (
+                        <li key={row + String(idx)}>{row}</li>
+                      ))}
+                    </ul>
+                  </Fragment>
+                ))}
+              </ul>
+            </div>
+          </ContentBox>
         </div>
-      </ContentBox>
-      <ContentBox title="About" iconSrc='img/about-icon.png'>
-        <div className='about-container'>
-          <p>Servicing the Central Florida Area for over a decade, Central Florida Specialty Services provides quality workmanship in all of your relocation needs.  Our techs have received the highest level of training, allowing us to guarantee the safety of your most delicate posessions.</p>
-          <br/>
-          <p>CFL Specialty Services is licensed and insured</p>
+        <div className='content-box-container' ref={aboutRef}>
+          <ContentBox title="About" iconSrc='img/about-icon.png'>
+            <div className='about-container'>
+              <p>Servicing Volusia, Orange, and St. John’s county, Central Florida Specialty Services offers quality workmanship in all of your relocation specialty needs.  We provide on-site crating for your most delicate possessions. </p>
+              <br/>
+              <p>CFL Specialty Services is licensed and insured. Our techs have each gone through a thorough background-check, and have the necessary tools and experience to ensure the highest level of professionalism.</p>
+            </div>
+          </ContentBox>
         </div>
-      </ContentBox>
-      <ContentBox title="Contact Us" iconSrc='img/location-icon.png'>
-        <div className='contact-us-container'>
-          <Map />
-          <ContactInfo />
+        <div className='content-box-container' ref={contactRef}>
+          <ContentBox title="Contact Us" iconSrc='img/location-icon.png'>
+            <div className='contact-us-container'>
+              <Map />
+              <ContactInfo />
+            </div>
+          </ContentBox>
         </div>
-      </ContentBox>
-    </div>
-    <Footer />
-  </Fragment>
-)
+      </div>
+      <Footer />
+    </Fragment>
+  )
+}
 
 export default App
